23e85428047314fe606214866fedfdcecabddc6f
= <rtyenriques@gmail.com>
2021-09-17 22:56:54 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,17 @@ class PostsController < ApplicationController
   end
 
   def update
+
     @post = Post.find(params[:id])
+    
 
     @post.update(post_params)
+    if @post.valid?
 
     redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
